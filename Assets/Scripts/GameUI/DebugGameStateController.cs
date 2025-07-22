9842ba47079788bc19f3cd0b083720d67037b58f
Pruland-Game <ai.ni.games02@gmail.com>
2025-07-23 08:34:09 +0900

    デバッグモード表示 + 文字位置画面上部に

--- Assets/Scripts/GameUI/DebugGameStateController.cs
+++ Assets/Scripts/GameUI/DebugGameStateController.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
+using TMPro;
 
-public class DebugGameStateController
+public class DebugGameStateController : MonoBehaviour
 {
-    
+    [SerializeField] private TextMeshProUGUI debugText;
+
+    private void Start()
+    {
+        if (debugText != null)
+        {
+            debugText.text = "Debug Mode";
+            debugText.gameObject.SetActive(true);
+        }
+    }
 }
